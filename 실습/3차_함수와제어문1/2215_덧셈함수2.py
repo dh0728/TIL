74a5199e1f5ac8e0625c1094564e15888b45d4eb
dh0728 <dustkscjswo@naver.com>
2024-07-21 23:56:08 +0900

    실습1

---
+++ 실습/3차_함수와제어문1/2215_덧셈함수2.py
@@ -0,0 +1,11 @@
+# 아래 함수를 수정하시오.
+def add_numbers(num1,num2):
+    return num1+num2
+
+
+
+# 수정한 add_numbers() 함수를 호출하시오.
+num1=3
+num2=5
+result =add_numbers(num1,num2)
+print(f'{num1}과 {num2}를 인자로 넘긴 경우,\n{result}')

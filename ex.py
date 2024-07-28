47f68601bbfe6f4ee945dd4606900d3af04e469d
dh0728 <dustkscjswo@naver.com>
2024-07-29 01:11:26 +0900

    729

--- ex.py
+++ ex.py
@@ -14,21 +14,24 @@
 
 # greet(30)
 
-A=[1,2,3,4]
-B=[5,6,7,8]
-C= list(zip(B))
-# print(C)
-scores = [
-    [10,20,30],
-    [40,50,39],
-    [20,40,50]
-]
+# A=[1,2,3,4]
+# B=[5,6,7,8]
+# C= list(zip(B))
+# # print(C)
+# scores = [
+#     [10,20,30],
+#     [40,50,39],
+#     [20,40,50]
+# ]
 
 # for score in zip(*scores):
 #     print(score)
 
-def my_function(x,y,z):
-    print(x,y,z)
+# def my_function(x,y,z):
+#     print(x,y,z)
 
-my_dict = {'x':1,'y':2, 'z':3}
-my_function(**my_dict)
+# my_dict = {'x':1,'y':2, 'z':3}
+# my_function(**my_dict)
+
+data1= [[0]*(10) for _ in range(10)]
+print(data1)
